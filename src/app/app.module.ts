56f878a85331fadb35d945069c9cef0b859c7ebd
Pablo Gonzalez <pablojuangonzalez@hotmail.com>
2022-07-06 02:43:49 -0300

    changing sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorizationComponent } from './components/authorization/authorization.component';
-import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 'angularx-social-login';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,25 +12,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SocialLoginModule,
-    ReactiveFormsModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider('32940002090-235pc86ieijhoa8r95659bdkunb2drqr.apps.googleusercontent.com'),
-          },
-        ],
-      } as SocialAuthServiceConfig,
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-authorization',
@@ -8,32 +6,20 @@ import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-soc
   styleUrls: ['./authorization.component.css']
 })
 export class AuthorizationComponent implements OnInit {
-  loginForm!: FormGroup;
-  socialUser!: SocialUser;
-  isLoggedin?: boolean = false;
-  constructor(
-    private formBuilder: FormBuilder,
-    private socialAuthService: SocialAuthService
-  ) {}
+
+  constructor() {}
+
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-    this.socialAuthService.authState.subscribe((user) => {
-      if(!this.isLoggedin) {
-        this.socialUser = user;
-        
-        window.location.href = `opaque://authorization/?name=${this.socialUser.name}&photo=${this.socialUser.photoUrl}&token=${this.socialUser.authToken}&email=${this.socialUser.email}`;
-        window.close();
-      }
-      this.isLoggedin = user != null;
-    });
+    this.addGoogleScript();
   }
-  loginWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-  logOut(): void {
-    this.socialAuthService.signOut();
+
+  addGoogleScript() {
+    let scriptDiv = document.createElement('script');
+
+    scriptDiv.setAttribute('async', '');
+    scriptDiv.setAttribute('defer', '');
+    scriptDiv.setAttribute('src', 'https://accounts.google.com/gsi/client');
+
+    document.body.appendChild(scriptDiv);
   }
 }
